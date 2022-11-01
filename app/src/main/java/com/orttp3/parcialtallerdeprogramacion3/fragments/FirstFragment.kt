0b29b2ccfda4173af8bd2da11fd51d57e73a6913
Magicoders <patopaiar@gmail.com>
2022-11-01 20:31:48 -0300

    Created folders

--- app/src/main/java/com/orttp3/parcialtallerdeprogramacion3/fragments/FirstFragment.kt
+++ app/src/main/java/com/orttp3/parcialtallerdeprogramacion3/fragments/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.orttp3.parcialtallerdeprogramacion3
+package com.orttp3.parcialtallerdeprogramacion3.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.orttp3.parcialtallerdeprogramacion3.R
 import com.orttp3.parcialtallerdeprogramacion3.databinding.FragmentFirstBinding
 
 /**
